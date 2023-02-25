d6c7cead89f1aa068a65aa619bc6ce4573a18697
rleivap <informaticaredlam@gmail.com>
2023-02-25 12:41:55 -0500

    primer commit proyecto citas-react

---
+++ src/components/Error.jsx
@@ -0,0 +1,10 @@
+
+const Error = ({ children }) => {
+    return (
+        <div className="bg-red-800 text-white text-center font-bold rounded-md uppercase p-3 mb-3">
+            {children}
+        </div>
+    )
+}
+
+export default Error
